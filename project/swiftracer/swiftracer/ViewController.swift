50bae88dd1eb49a318b2bc14254da2f1fd1a389b
Khrob Edmonds <khrob@accomplio.com>
2015-10-07 22:53:05 -0700

    Added some ui
     - horizontal 'distance' to window
     - verticals are camera x,y,z values

--- project/swiftracer/swiftracer/ViewController.swift
+++ project/swiftracer/swiftracer/ViewController.swift
@@ -12,14 +12,29 @@ class ViewController: NSViewController
 {
     @IBOutlet weak var image: BackgroundImage!
     
+    @IBOutlet weak var xSlider: NSSlider!
+    @IBOutlet weak var ySlider: NSSlider!
+    @IBOutlet weak var zSlider: NSSlider!
+
+    @IBOutlet weak var distanceSlider: NSSlider!
+
+    let width =  400
+    let height = 300
+    
+    @IBAction func uiChanged(sender: AnyObject)
+    {
+        print ("Redrawing (\(xSlider.doubleValue),\(ySlider.doubleValue),\(zSlider.doubleValue)) \(distanceSlider.doubleValue)")
+        let rays = makeRays(width, height: height, origin: Vector(x:xSlider.doubleValue, y:ySlider.doubleValue, z:zSlider.doubleValue), distance: distanceSlider.doubleValue, fov: 90.0)
+        let buffer = simpleRaycast(rays, width:width, height:height)
+        let gradient = imageFromARGB32Bitmap (buffer, width:width, height:height)
+        
+        image.image = gradient
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
-        let buffer = simpleGradient(480,height: 300)
-        let gradient = imageFromARGB32Bitmap (buffer, width: 480, height: 300)
-        
-        image.image = gradient
+        uiChanged(self)
     }
     
 }
